733cc5c678ca656aada96ad80a7439e9de911fff
Auralion <abdelhamidou4@gmail.com>
2024-02-19 13:24:05 +0100

    add create quiz route and fix database structure for quiz

---
+++ backend/src/controllers/quiz/create-quiz.dto.ts
@@ -0,0 +1,12 @@
+import { IsString } from 'class-validator';
+
+/**
+ * Data transfer object for creating a quiz
+ */
+export class CreateQuizDto {
+  /**
+   * The name of the quiz
+   */
+  @IsString()
+  name: string;
+}
--- backend/src/controllers/quiz/quiz.controller.ts
+++ backend/src/controllers/quiz/quiz.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req, Res } from '@nestjs/common';
 import { Auth } from 'src/decorators/auth.decorator';
 import { QuizService } from 'src/services/quiz/quiz.service';
+import { CreateQuizDto } from './create-quiz.dto';
+import { Request, Response } from 'express';
 
 @Controller('quizzes')
 @Auth()
@@ -16,5 +18,17 @@ export class QuizController {
     }
 
     return this.quizService.findMany(page, search);
+  }
+
+  @Post()
+  public async createQuiz(
+    @Body() data: CreateQuizDto,
+    @Req() request: Request,
+    @Res() response: Response,
+  ) {
+    const author = request.user!;
+    await this.quizService.create(data.name, author);
+
+    return response.status(201).send();
   }
 }
